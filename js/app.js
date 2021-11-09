f1985270e1f9193efd6a869eff01124842ffa890
Anna <protsenko.anyta@gmail.com>
2021-11-09 22:49:49 +0100

    pagination done, comment form add

--- js/app.js
+++ js/app.js
@@ -1,33 +1,46 @@
-const apiUrl = "https://jordan.ashton.fashion/api/goods/30/comments";
+const apiUrl = "https://jordan.ashton.fashion/api/goods/30/comments?page=";
+const paginationList = document.querySelector(".pagination");
+const commentList = document.querySelector(".comment-list");
+
 async function getCommentsData(page = 1) {
-  const dataObj = await fetch(`${apiUrl}?page=${page}`).then((response) =>
+  const dataObj = await fetch(`${apiUrl}${page}`).then((response) =>
     response.json()
   );
+  commentList.innerHTML = "";
   dataObj.data.forEach((comment, index) => {
     renderComment(comment, index);
   });
+  paginationList.innerHTML = "";
   dataObj.links.forEach((link, index) => {
     renderLink(link, index);
   });
 }
-getCommentsData(4);
+getCommentsData(1);
 
 function renderComment(comment, index) {
-  const commentList = document.querySelector(".comment-list");
   const commentWrapper = document.createElement("div");
   commentWrapper.classList.add("comment-wrapper");
   const nameOfComment = document.createElement("p");
   nameOfComment.classList.add("comment-name");
-  nameOfComment.textContent = `${comment.name}`;
+  nameOfComment.textContent = comment.name;
   commentWrapper.appendChild(nameOfComment);
-  const textOfComment = document.createElement("p");
+  const textOfComment = document.createElement("span");
   textOfComment.classList.add("comment-text");
-  textOfComment.textContent = `${comment.text}`;
+  textOfComment.textContent = comment.text;
   commentWrapper.appendChild(textOfComment);
   commentList.appendChild(commentWrapper);
   //   console.log(` ${index} ${comment.name} ${comment.text}`);
 }
 
 function renderLink(link, index) {
+  const pageButton = document.createElement("button");
+  pageButton.classList.add("btn-pages");
+  pageButton.innerHTML = link.label;
+  paginationList.appendChild(pageButton);
+
+  pageButton.addEventListener("click", () => {
+    let [, pageNumber] = link.url.split("?page=");
+    getCommentsData(pageNumber);
+  });
   console.log(`${index} ${link.label} ${link.url}`);
 }
