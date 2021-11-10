80452a18550c7f7e0c8d4d87e75eb10ab6c41f78
Anna <protsenko.anyta@gmail.com>
2021-11-10 01:34:18 +0100

    post comment, validity, pagination

--- js/app.js
+++ js/app.js
@@ -1,12 +1,21 @@
-const apiUrl = "https://jordan.ashton.fashion/api/goods/30/comments?page=";
+const apiUrl = "https://jordan.ashton.fashion/api/goods/30/comments";
 const paginationList = document.querySelector(".pagination");
 const commentList = document.querySelector(".comment-list");
+const btnLoadMore = document.querySelector(".btn-load-more");
+const commentForm = document.querySelector(".comment-form");
+let currentPageNumber = null;
+let nextPageNumber = null;
+
+function removeComments() {
+  commentList.innerHTML = "";
+}
+
+btnLoadMore.addEventListener("click", () => getCommentsData(nextPageNumber));
 
 async function getCommentsData(page = 1) {
-  const dataObj = await fetch(`${apiUrl}${page}`).then((response) =>
+  const dataObj = await fetch(`${apiUrl}?page=${page}`).then((response) =>
     response.json()
   );
-  commentList.innerHTML = "";
   dataObj.data.forEach((comment, index) => {
     renderComment(comment, index);
   });
@@ -14,6 +23,25 @@ async function getCommentsData(page = 1) {
   dataObj.links.forEach((link, index) => {
     renderLink(link, index);
   });
+  // button load next page
+
+  const linksCount = dataObj.links.length;
+  const lastLink = dataObj.links[linksCount - 1];
+  nextPageNumber = getPageFromLink(lastLink);
+
+  btnLoadMore.hidden = nextPageNumber === null;
+  const currentLink = dataObj.links.find((link) => link.active === true);
+  currentPageNumber = getPageFromLink(currentLink);
+  // current page
+}
+
+function getPageFromLink(link) {
+  if (link.url === null) {
+    return null;
+  } else {
+    const [, pageNumber] = link.url.split("?page=");
+    return pageNumber;
+  }
 }
 getCommentsData(1);
 
@@ -34,13 +62,39 @@ function renderComment(comment, index) {
 
 function renderLink(link, index) {
   const pageButton = document.createElement("button");
+  if (link.active) {
+    pageButton.classList.add("active");
+  }
   pageButton.classList.add("btn-pages");
   pageButton.innerHTML = link.label;
   paginationList.appendChild(pageButton);
-
   pageButton.addEventListener("click", () => {
-    let [, pageNumber] = link.url.split("?page=");
+    let pageNumber = getPageFromLink(link);
+    removeComments();
     getCommentsData(pageNumber);
   });
   console.log(`${index} ${link.label} ${link.url}`);
+}
+
+commentForm.addEventListener("submit", postComment);
+
+async function postComment(event) {
+  if (!commentForm.checkValidity()) {
+    return;
+  }
+  event.preventDefault();
+  let formData = new FormData(commentForm);
+  let formDataJson = JSON.stringify(Object.fromEntries(formData));
+  let result = await fetch(apiUrl, {
+    method: "POST",
+    redirect: "follow",
+    body: formDataJson,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.text());
+
+  console.log("💎" + result);
+  removeComments();
+  getCommentsData(currentPageNumber);
 }
